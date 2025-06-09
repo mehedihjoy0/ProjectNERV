beeca22ba2b6d65823481318a57005580446fa1c
ocin4ever <85343782+Ocin4ever@users.noreply.github.com>
2025-06-09 23:40:14 +0300

    unica: patches: knoxpatch: initialize from ExtremeROM Co-Authored-By: Salvo Giangreco <giangrecosalvo9@gmail.com>

--- unica/mods/knoxpatch/customize.sh
+++
@@ -1,9 +0,0 @@
-DELETE_FROM_WORK_DIR "system" "system/etc/public.libraries-wsm.samsung.txt"
-DELETE_FROM_WORK_DIR "system" "system/lib/libhal.wsm.samsung.so"
-DELETE_FROM_WORK_DIR "system" "system/lib/vendor.samsung.hardware.security.wsm.service-V1-ndk.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libhal.wsm.samsung.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.security.wsm.service-V1-ndk.so"
-
-if [ -f "$WORK_DIR/system/system/priv-app/KmxService/KmxService.apk" ]; then
-    APPLY_PATCH "system" "system/priv-app/KmxService/KmxService.apk" "$SRC_DIR/unica/mods/knoxpatch/kmx/KmxService.apk/0001-Nuke-ROT-IntegrityStatus-check.patch"
-fi
