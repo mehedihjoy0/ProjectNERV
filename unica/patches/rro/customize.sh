d5de2d081373eccf2719dc6acbb41b2423cb8677
Yagzie <yagzie11@gmail.com>
2025-06-09 22:54:03 +0300

    unica: patches: rro: adapt for One UI 7

--- unica/patches/rro/customize.sh
+++ unica/patches/rro/customize.sh
@@ -1,9 +1,9 @@
 if [[ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]]; then
-    DECODE_APK "product" "overlay/framework-res__auto_generated_rro_product.apk"
+    DECODE_APK "product" "overlay/framework-res__dm1qxxx__auto_generated_rro_product.apk"
 
     echo "Applying stock overlay configs"
-    rm -rf "$APKTOOL_DIR/product/overlay/framework-res__auto_generated_rro_product.apk/res"
+    rm -rf "$APKTOOL_DIR/product/overlay/framework-res__dm1qxxx__auto_generated_rro_product.apk/res"
     cp -a --preserve=all \
         "$SRC_DIR/target/$TARGET_CODENAME/overlay" \
-        "$APKTOOL_DIR/product/overlay/framework-res__auto_generated_rro_product.apk/res"
+        "$APKTOOL_DIR/product/overlay/framework-res__dm1qxxx__auto_generated_rro_product.apk/res"
 fi
