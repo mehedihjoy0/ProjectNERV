8915a24f64ef91399a0d030af59588f35fdebc2f
Yagzie <yagzie11@gmail.com>
2025-06-11 16:07:22 +0300

    unica: mods: settings: remove patches that are not ready

--- unica/mods/settings/customize.sh
+++
@@ -1,5 +0,0 @@
-SET_PROP "system" "ro.unica.version" "$ROM_VERSION"
-SET_PROP "system" "ro.unica.codename" "$ROM_CODENAME"
-
-DECODE_APK "system" "system/priv-app/SecSettings/SecSettings.apk"
-cp -a --preserve=all "$SRC_DIR/unica/mods/settings/SecSettings.apk/"* "$APKTOOL_DIR/system/priv-app/SecSettings/SecSettings.apk"
