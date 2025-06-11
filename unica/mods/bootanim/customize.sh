754e893f36ca2db37e061da94585fb6f840f51fe
Yagzie <yagzie11@gmail.com>
2025-06-11 16:56:51 +0300

    unica: mods: bootanim: add S25 sounds with this module as well

--- unica/mods/bootanim/customize.sh
+++ unica/mods/bootanim/customize.sh
@@ -1,5 +1,5 @@
-TWOTHREE_TARGETS="dm1q dm2q g0q r0q"
-TWOFOUR_TARGETS="a52q a52xq a52sxq a71 a72q a73xq m52xq r8q r9q r9q2"
+TWOTHREE_TARGETS=""
+TWOFOUR_TARGETS="a52sxq a73xq m52xq r8q r9q r9q2"
 
 if echo "$TWOTHREE_TARGETS" | grep -q -w "$TARGET_CODENAME"; then
     cp -a --preserve=all "$SRC_DIR/unica/mods/bootanim/2340x1080/"* "$WORK_DIR/system/system/media"
@@ -8,3 +8,7 @@ elif echo "$TWOFOUR_TARGETS" | grep -q -w "$TARGET_CODENAME"; then
 else
     echo "Unknown boot animation resolution for \"$TARGET_CODENAME\""
 fi
+
+echo "Adding S25 Sounds"
+DELETE_FROM_WORK_DIR "system" "system/media/audio"
+ADD_TO_WORK_DIR "pa1qxx" "system" "system/media/audio"
