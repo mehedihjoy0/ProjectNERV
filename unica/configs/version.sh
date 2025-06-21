95e7d0dacd7636b8033630c045ee79d89ab466ba
Yagzie <97476482+Yagzie@users.noreply.github.com>
2025-06-21 22:39:29 +0300

    unica: configs: version: gurt: yo
    1.0.3 --> 1.1.0

--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -16,9 +16,8 @@
 #
 
 VERSION_MAJOR=1
-VERSION_MINOR=0
-VERSION_PATCH=3
-
+VERSION_MINOR=1
+VERSION_PATCH=0
 ROM_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
 # Append current commit hash to version name
 ROM_VERSION+="-$(git rev-parse --short HEAD)"
