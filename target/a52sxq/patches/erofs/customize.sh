750b04d2b7ab75fdef53e9a052cdcc3a8d47f54a
Saad <saadelasfur@gmail.com>
2025-07-13 18:36:30 +0300

    target: all: patches: erofs: init
    Required by ef6dd8a09c102b60d60300527fc8ea8e3e320dbf.
    
    Signed-off-by: Saad <saadelasfur@gmail.com>

---
+++ target/a52sxq/patches/erofs/customize.sh
@@ -0,0 +1,8 @@
+SKIPUNZIP=1
+
+if $TARGET_FS_SWITCHED_TO_EROFS; then
+    LOG "- Copying patched fstab to /vendor/etc"
+    cp -f "$SRC_DIR/target/$TARGET_CODENAME/patches/erofs/vendor/etc/fstab.qcom" "$WORK_DIR/vendor/etc"
+else
+    LOG "- TARGET_OS_FILE_SYSTEM is not EROFS. Ignoring"
+fi
