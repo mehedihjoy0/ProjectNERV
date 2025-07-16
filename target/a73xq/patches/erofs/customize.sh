812caeae72031c6016d09448ce52cdb7e50c0c2f
Saad <saadelasfur@gmail.com>
2025-07-16 21:05:37 +0300

    target: all: follow up of 70429950
    Signed-off-by: Saad <saadelasfur@gmail.com>

--- target/a73xq/patches/erofs/customize.sh
+++
@@ -1,8 +0,0 @@
-SKIPUNZIP=1
-
-if $TARGET_FS_CHANGED; then
-    LOG "- Copying patched fstab to /vendor/etc"
-    cp -f "$SRC_DIR/target/$TARGET_CODENAME/patches/erofs/vendor/etc/fstab.qcom" "$WORK_DIR/vendor/etc"
-else
-    LOG "- TARGET_OS_FILE_SYSTEM is not EROFS. Ignoring"
-fi
